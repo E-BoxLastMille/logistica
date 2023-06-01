3b102d3d1d80fba0d4b73380cd7c6b3cf86ccb2a
heykhryst <christophoer.rodas@e-box.es>
2023-06-01 20:12:19 +0000

    Módulo E-Box

---
+++ e-box/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "E-Box",
+
+    'summary': """
+        Personalizaciones E-Box""",
+
+    'description': """
+        Personalizaciones E-Box
+    """,
+
+    'author': "Voodoo Enterprise Software S.L.",
+    'website': "https://www.voodoo.es",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '16.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+    ],
+}
