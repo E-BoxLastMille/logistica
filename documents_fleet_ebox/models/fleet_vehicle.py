c092c41aef48a5a2e5598aec07d78e74d26d8c32
gaso <gustavo2840@gmail.com>
2025-08-20 19:03:45 -0500

    [MIG] *: ajuste en modulos base para preparar premigracion. #task=6723

--- documents_fleet_ebox/models/fleet_vehicle.py
+++ documents_fleet_ebox/models/fleet_vehicle.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import logging
-
 from odoo import api, fields, models
 
 
--- documents_fleet_ebox/models/res_config_settings.py
+++ documents_fleet_ebox/models/res_config_settings.py
@@ -6,10 +6,15 @@ from odoo import api, fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    documents_fleet_settings = fields.Boolean(
-        related='company_id.documents_fleet_settings', readonly=False, string="Fleet Resources")
-    documents_fleet_folder = fields.Many2one(
-        'documents.folder', related='company_id.documents_fleet_folder', readonly=False, string="fleet default workspace")
+    def _domain_company_fleet(self):
+        company = self.env.company
+        return ['|', ('company_id', '=', False), ('company_id', '=', company.id), ('type', '=', 'folder')]
+
+    documents_fleet_settings = fields.Boolean(related='company_id.documents_fleet_settings',
+                                              readonly=False, string="Fleet Resources")
+    documents_fleet_folder = fields.Many2one('documents.document', related='company_id.documents_fleet_folder',
+                                             readonly=False, domain=_domain_company_fleet,
+                                             string="fleet default workspace")
 
     @api.onchange('documents_fleet_folder')
     def _onchange_documents_fleet_folder(self):
